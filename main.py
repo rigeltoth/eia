96a8b158c003c811679191d7987dfd2837788072
Ronnovič Rigel Toth <rigel.toth@gmail.com>
2025-10-06 21:36:43 -0500

    [main] simple model added

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from transformers import pipeline
 
+summarizer = pipeline("summarization", model="google/flan-t5-base")
 app = FastAPI()
 
+class ReviewsInput(BaseModel):
+    reviews: List[str]
 
-@app.get("/")
-def ping():
-   return "pong"
+@app.post("/reviews")
+def reviews(data: ReviewsInput):
+    combined_text = " ".join(data.reviews)
+    summary = summarizer(combined_text, max_length=150, min_length=40, do_sample=False)
+    return {"summary": summary[0]['summary_text']}
